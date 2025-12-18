deb42b065dbd3d203cab7b2f25e682b192f802fa
Linh <ntlinh1892004@gmail.com>
2025-12-18 15:29:27 +0700

    fix: remove some libraries from dependencies

--- src/config/solana/program.ts
+++ src/config/solana/program.ts
@@ -1,27 +1,27 @@
 import { Connection } from "@solana/web3.js";
 import { Solana, chain } from "../chain.ts";
-import * as anchor from "@coral-xyz/anchor";
+import { Provider, Program, AnchorProvider } from "@coral-xyz/anchor";
 import solanaClientIdl from './idl/solana_client.json';
 import { SolanaClient } from './types/solana_client.ts';
 
 export interface SolanaOptions {
   connection?: Connection;
   wallet?: any;
-  provider?: anchor.Provider;
+  provider?: Provider;
 }
 
-export const solanaClientProgram = (chainType: Solana, options: SolanaOptions = {}): anchor.Program<SolanaClient> => {
+export const solanaClientProgram = (chainType: Solana, options: SolanaOptions = {}): Program<SolanaClient> => {
   let provider = options.provider;
   if (!provider) {
-    provider = new anchor.AnchorProvider(
+    provider = new AnchorProvider(
       options.connection ? options.connection : chain[chainType].connection!,
       options.wallet ? options.wallet : {} as any,
       { commitment: "confirmed" }
     );
   }
 
-  return new anchor.Program(
+  return new Program(
     solanaClientIdl,
     provider
-  ) as anchor.Program<SolanaClient>;
+  ) as Program<SolanaClient>;
 }
